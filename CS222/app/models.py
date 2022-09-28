fa7fcc2b277571ed655928e670e452d96e2a855d
Aditya Matiwala <adi.matiwala@gmail.com>
2022-09-28 12:42:46 -0500

    Added email/password identifier fields

--- CS222/app/models.py
+++ CS222/app/models.py
@@ -1,7 +1,10 @@
+import email
 from django.db import models
 
 class Student(models.Model):
-    name = models.CharField(max_length = 150, null = True)
+    email = models.CharField(max_length = 150, null = True)
+    password = models.CharField(max_length = 150, null = True)
+    full_name = models.CharField(max_length = 150, null = True)
     major = models.CharField(max_length = 150, null = True)
     hobbies = models.CharField(max_length = 300, null = True)
     classes = models.CharField(max_length = 150, null = True)
