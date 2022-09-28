d8df6df21189467fb1fd4cead9b32898023b44f0
erin-yiran-wang <erinwang27y@gmail.com>
2022-09-28 01:32:08 -0500

    front end login page

---
+++ src/components/App/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+
+// function setToken(userToken) {
+//   sessionStorage.setItem('token', JSON.stringify(userToken));
+// }
+
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+//   const userToken = JSON.parse(tokenString);
+//   return userToken?.token
+// }
+
+function App() {
+  const [token, setToken] = useState();
+  // const token = getToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" 
+            element = { <Dashboard /> }>
+          </Route>
+          <Route path="/preferences"
+            element = {<Preferences />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
