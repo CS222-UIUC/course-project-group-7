8412e260dec84acaf0bc2843eb3a94295e975420
Jalen Xing <66393515+Jalen12345@users.noreply.github.com>
2022-09-30 01:33:55 -0500

    Add files via upload

--- CS222/app/models.py
+++ CS222/app/models.py
@@ -1,11 +1,12 @@
-import email
 from django.db import models
 
 class Student(models.Model):
-    email = models.CharField(max_length = 150, null = True)
-    password = models.CharField(max_length = 150, null = True)
-    full_name = models.CharField(max_length = 150, null = True)
+    name = models.CharField(max_length = 150, null = True)
     major = models.CharField(max_length = 150, null = True)
     hobbies = models.CharField(max_length = 300, null = True)
     classes = models.CharField(max_length = 150, null = True)
     social_media = models.CharField(max_length = 300, null = True)
+    email = models.CharField(max_length = 300, null = True)
+
+    def __str__(self):
+        return self.email + ' ' + self.major + ' ' + self.hobbies + ' ' + self.classes + ' ' + self.social_media
