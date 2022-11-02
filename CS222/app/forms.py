5f170db8327b0642f0c062c0e6f6e1cee86ff0ae
Jalen Xing <66393515+Jalen12345@users.noreply.github.com>
2022-11-02 01:57:59 -0500

    Add files via upload

--- CS222/app/forms.py
+++ CS222/app/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.forms import ModelForm
 from .models import Student
+from django import forms
 
 
 class CreateUserForm(UserCreationForm):
@@ -12,7 +13,16 @@ class CreateUserForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class StudentForm(ModelForm):
+class StudentForm(forms.ModelForm):
     class Meta:
         model = Student
         fields = ['name', 'major', 'hobbies', 'classes', 'social_media', 'email']
+        widget = {
+            'name' : forms.TextInput(attrs= {'class' : 'form-control'}),
+            'major' : forms.TextInput(attrs= {'class' : 'form-control'}),
+            'hobbies' : forms.TextInput(attrs= {'class' : 'form-control'}),
+            'classes' : forms.TextInput(attrs= {'class' : 'form-control'}),
+            'social_media' : forms.TextInput(attrs= {'class' : 'form-control'}),
+            'email' : forms.TextInput(attrs= {'class' : 'form-control'}),
+
+        }
