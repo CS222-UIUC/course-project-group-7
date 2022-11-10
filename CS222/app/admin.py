eb6ca142bf12458d5cb37126672172dffcc3ea9b
Jalen Xing <66393515+Jalen12345@users.noreply.github.com>
2022-11-10 03:18:25 -0600

    Add files via upload

--- CS222/app/admin.py
+++ CS222/app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student
+from .models import Student, UserStudent
 
 admin.site.register(Student)
+admin.site.register(UserStudent)
