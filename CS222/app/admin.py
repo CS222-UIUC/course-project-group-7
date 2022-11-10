408f698607c86171f2717b851cb81cef5ba65a15
Jalen Xing <66393515+Jalen12345@users.noreply.github.com>
2022-11-10 03:16:13 -0600

    Revert "Add files via upload"

--- CS222/app/admin.py
+++ CS222/app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, UserStudent
+from .models import Student
 
 admin.site.register(Student)
-admin.site.register(UserStudent)
