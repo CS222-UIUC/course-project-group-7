3ce7e73ebaf618ba958cfdcc52a25c4ba0d38a04
Jalen Xing <66393515+Jalen12345@users.noreply.github.com>
2022-11-18 00:12:39 -0600

    Add files via upload

--- CS222/app/views.py
+++ CS222/app/views.py
@@ -3,8 +3,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
-from .forms import StudentForm
-from .models import Student
+from .forms import StudentForm, UserForm
+from .models import Student, UserStudent
+# import os
 # Create your views here.
 
 
@@ -59,9 +60,10 @@ def logoutUser(request):
 def home(response):
     if response.method == "POST":
         form = StudentForm(response.POST)
+        userForm = UserForm(response.POST)
         if form.is_valid:
             form.save()
-
+            userForm.save()
             return redirect('profiles')
     else:
         form = StudentForm()
@@ -73,3 +75,34 @@ def studentProfile(response):
     student_list = Student.objects.all()
 
     return render(response, 'register/student_profile.html', {'student_list': student_list})
+
+
+def filterStudentProfile(response):
+    student_list = Student.objects.all()
+
+    user_list = UserStudent.objects.all()
+
+    match_class = user_list[0].classes.split(",")
+    temp = []
+    for student in student_list:
+        for class_ in match_class:
+            if class_.strip().upper() in student.classes:
+                temp.append(student)
+                break
+
+    return render(response, 'register/filtered_profiles.html', {'student_list': temp})
+
+
+def filterStudentHobbies(response):
+    student_list = Student.objects.all()
+    user_list = UserStudent.objects.all()
+
+    match_hobbie = user_list[0].hobbies.split(",")
+    temp = []
+    for student in student_list:
+        for hobbie in match_hobbie:
+            if hobbie.strip().lower() in student.hobbies:
+                temp.append(student)
+                break
+
+    return render(response, 'register/filtered_hobbies.html', {'student_list': temp})
